6025ba954e1b88eda1760c4c1a0b0364783f17a2
Jef Kazimer <11558645+JefTek@users.noreply.github.com>
2024-05-23 13:51:25 -0700

    Update Update-GuestUserSponsorsFromInvitedBy.ps1
    test 1

--- code/ps/Update-GuestUserSponsorsFromInvitedBy.ps1
+++ code/ps/Update-GuestUserSponsorsFromInvitedBy.ps1
@@ -1,11 +1,25 @@
+<#
+.Synopsis
+   Update the Sponsors attribute to include the user who initially invited them to the tenant
+.DESCRIPTION
+   Update the Sponsors attribute to include the user who initially invited them to the tenant
+.EXAMPLE
+   Update-GuestUserSponsorsFromInvitedBy -All
+.EXAMPLE
+   Update-GuestUserSponsorsFromInvitedBy -UserId user1@contoso.com,user2@contoso.com
+#>
 function Update-GuestUserSponsorsFromInvitedBy {
-    [CmdletBinding(SupportsShouldProcess = $True)]
+    [CmdletBinding(SupportsShouldProcess,
+        ConfirmImpact = 'High',
+        DefaultParameterSetName = 'AllInvitedGuests')]
     param (
 
         # UserId of Guest User
+        [Parameter(ParameterSetName = 'ByUsers')]
         [String[]]
         $UserId,
         # Enumerate and Update All Guest Users
+        [Parameter(ParameterSetName = 'AllInvitedGuests')]
         [switch]
         $All
     )
@@ -33,13 +47,57 @@ function Update-GuestUserSponsorsFromInvitedBy {
         }
         else {
             foreach ($guestUser in $GuestUsers) {
-                #eval and update guest
+                $invitedBy = $null
+
+                $splatArgumentsGetInvitedBy = @{
+
+                    Method = 'Get'
+                    Uri    = ("https://graph.microsoft.com/beta/users/" + $guestUser.Id + "/invitedBy")
+
+
+                }
+
+                $invitedBy = Invoke-MgGraphRequest @splatArgumentsGetInvitedBy
+
+                Write-Verbose ("InvitedBy for Guest User {0}: {1}" -f $guestuser.DisplayName, $invitedBy.value.id)
+
+                if ($null -ne $invitedBy) {
+                    if ($guestUser.Sponsors -notcontains $invitedBy) {
+                        write-verbose ("Sponsors does not contain the user who invited them!")
+
+                        if ($PSCmdlet.ShouldProcess(("{0} - {1}" -f $guestUser.displayName, $guestUser.id), "Update Sponsors")) {
+                            try {
+
+
+                                $sponsorUrl = ("https://graph.microsoft.com/beta/users/{0}" -f $invitedBy.value.id)
+                                $dirObj = @{"sponsors@odata.bind" = @($sponsorUrl) }
+                                $sponsorsRequestBody = $dirObj | ConvertTo-Json
+
+
+                                Update-MgBetaUser -UserId $guestUser.Id  -BodyParameter $sponsorsRequestBody
+
+
+                            }
+                            catch {
+
+                                Write-Error $_
+
+                            }
+                        }
+
+
+                    }
+                }
+
+
             }
         }
 
     }
 
     end {
+
+        Write-Verbose "Complete!"
 
     }
 }
